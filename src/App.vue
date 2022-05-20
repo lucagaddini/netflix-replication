f1ee16e9af235d81e69bbde858e7412c91386294
lucagad <90861515+lucagad@users.noreply.github.com>
2022-05-20 11:40:45 +0200

    {Vue} Added: API call inside the App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,41 @@
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
 
+import axios from 'axios';
+
 export default {
   name: 'App',
   components: {
     HeaderComponent,
     MainComponent
+  },
+  data(){
+    return{
+      apiURL:'https://api.themoviedb.org/3/search/movie/',
+      apiParameters: {
+        api_key: '933535a20fccede2394fcd6641cbed47',
+        language: 'it-IT',
+        query: 'harry potter'
+      }
+    }
+  },
+  mounted(){
+      this.getAPI();
+    },
+    methods:{
+      getAPI(){
+        axios.get(this.apiURL,{
+          params: this.apiParameters
+        })
+        .then(r => {
+          this.albumArray = r.data.response;
+          console.log('Risposta API ---->',r.data);
+        })
+      }
+    },
 }
-}
+
+
 </script>
 
 
