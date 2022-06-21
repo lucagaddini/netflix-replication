920d95ad27391518241f3261d856ffb17a1bdc50
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-06-21 13:55:33 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 // vue.config.js file to be placed in the root of your repository
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-boolflix/'
+    ? '/'
     : '/'
 }
 
