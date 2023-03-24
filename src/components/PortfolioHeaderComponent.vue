36c2e57d5df576a5b758dbbff61e45759aa45574
Luca Gaddini <90861515+lucagaddini@users.noreply.github.com>
2023-03-24 11:19:28 +0100

    Aggiornato link GitHub

--- src/components/PortfolioHeaderComponent.vue
+++ src/components/PortfolioHeaderComponent.vue
@@ -8,7 +8,7 @@
       </a>
       <ul class="navbar-nav me-auto d-none d-lg-flex mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link d-flex align-items-center me-4 tracking-wide" href="https://github.com/lucagad"><i style="font-size: 20px" class="bi bi-github me-2 align-middle"></i> GitHub</a>
+          <a class="nav-link d-flex align-items-center me-4 tracking-wide" href="https://github.com/lucagaddini"><i style="font-size: 20px" class="bi bi-github me-2 align-middle"></i> GitHub</a>
         </li>
         <li class="nav-item">
             <a class="nav-link d-flex align-items-center tracking-wide" href="https://www.linkedin.com/in/lucagaddini/"><i style="font-size: 20px" class="bi bi-linkedin me-2 align-middle"></i> Linkedin</a>
